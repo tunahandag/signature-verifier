1aedb83e4dd10353dba2af0af52a0718c70eb68a
Tunahan Dag <tunahandag@hotmail.com>
2023-06-17 11:29:45 +0300

    test: 💍 tests are implemented for nonce check and custom errors

--- test/SignatureVerifier.test.js
+++ test/SignatureVerifier.test.js
@@ -7,6 +7,14 @@ const { getEIP712Signature } = require('./common/utilities');
 const { SIGNING_DOMAIN_NAME, SIGNATURE_VERSION, CHAIN_ID } = require('./common/constants');
 
 describe('VerifySignature', function () {
+	const types = {
+		Buy: [
+			{ name: 'id', type: 'uint256' },
+			{ name: 'timestamp', type: 'uint256' },
+			{ name: 'buyer', type: 'address' },
+			{ name: 'nonce', type: 'uint256' },
+		],
+	};
 	const setupSignature = async (contractAddress, signer, buyerAddress, timestamp, id, nonce) => {
 		const domain = {
 			name: SIGNING_DOMAIN_NAME,
@@ -22,18 +30,35 @@ describe('VerifySignature', function () {
 			nonce: nonce,
 		};
 
-		const types = {
-			Buy: [
-				{ name: 'id', type: 'uint256' },
-				{ name: 'timestamp', type: 'uint256' },
-				{ name: 'buyer', type: 'address' },
-				{ name: 'nonce', type: 'uint256' },
-			],
-		};
-
 		const signature = await getEIP712Signature(domain, types, buy, signer);
 
 		return { ...buy, signature };
+	};
+	const recoverSignature = async (
+		contractAddress,
+		signature,
+		buyerAddress,
+		timestamp,
+		id,
+		nonce,
+	) => {
+		const domain = {
+			name: SIGNING_DOMAIN_NAME,
+			version: SIGNATURE_VERSION,
+			verifyingContract: contractAddress,
+			chainId: CHAIN_ID,
+		};
+
+		const buy = {
+			id: id,
+			timestamp: timestamp,
+			buyer: buyerAddress,
+			nonce: nonce,
+		};
+
+		const recoveredAddress = ethers.utils.verifyTypedData(domain, types, buy, signature);
+
+		return recoveredAddress;
 	};
 
 	let deployer, signer1, signer2;
@@ -86,25 +111,37 @@ describe('VerifySignature', function () {
 			signatureVerifier
 				.connect(signer2)
 				.verifySignature([id, timestamp, buyer, nonce], signature),
-		).to.revertedWithCustomError(signatureVerifier, 'SignatureAlreadyUsed');
+		)
+			.to.revertedWithCustomError(signatureVerifier, 'SignatureAlreadyUsed')
+			.withArgs(signature);
 	});
 	it('Signer1 signs the buy object of signer2 which must revert.', async function () {
 		const buyerNonce = await signatureVerifier.getNonce(signer2.address);
+		const buyTimestamp = Date.now();
 		const result = await setupSignature(
 			signatureVerifier.address,
 			signer1,
 			signer2.address,
-			Date.now(),
+			buyTimestamp,
 			3,
 			buyerNonce,
 		);
 		const { id, timestamp, buyer, nonce, signature } = result;
-
+		const recoveredAddress = await recoverSignature(
+			signatureVerifier.address,
+			signature,
+			buyer,
+			timestamp,
+			id,
+			nonce,
+		);
 		await expect(
 			signatureVerifier
 				.connect(signer2)
 				.verifySignature([id, timestamp, buyer, nonce], signature),
-		).to.revertedWithCustomError(signatureVerifier, 'InvalidSigner');
+		)
+			.to.revertedWithCustomError(signatureVerifier, 'InvalidSigner')
+			.withArgs(recoveredAddress, buyer);
 	});
 	it('A valid signature from signer1 can go through the verification.', async function () {
 		const buyerNonce = await signatureVerifier.getNonce(signer2.address);
@@ -148,10 +185,34 @@ describe('VerifySignature', function () {
 			signatureVerifier
 				.connect(signer2)
 				.verifySignature([id, timestamp, buyer, nonce], signature),
-		).to.revertedWithCustomError(signatureVerifier, 'SignatureExpired');
+		)
+			.to.revertedWithCustomError(signatureVerifier, 'SignatureExpired')
+			.withArgs(timestamp);
 
 		const newNonce = await signatureVerifier.getNonce(signer1.address);
 
 		await expect(newNonce).to.equal(buyerNonce);
+	});
+
+	it('A valid signature must revert if nonce is not correct ', async function () {
+		const buyerNonce = await signatureVerifier.getNonce(signer2.address);
+		// mine a new block with timestamp `newTimestamp`
+		const result = await setupSignature(
+			signatureVerifier.address,
+			signer2,
+			signer2.address,
+			Date.now() + 200000,
+			15,
+			buyerNonce + 1,
+		);
+		const { id, timestamp, buyer, nonce, signature } = result;
+
+		await expect(
+			signatureVerifier
+				.connect(signer2)
+				.verifySignature([id, timestamp, buyer, nonce], signature),
+		)
+			.to.revertedWithCustomError(signatureVerifier, 'InvalidNonce')
+			.withArgs(nonce, buyerNonce);
 	});
 });
